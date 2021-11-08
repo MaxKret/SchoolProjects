e14ae8337fe5334fe0fccd3d4c2612102a8748ef
Maxwell Kretschmer <maxwellkret@gmail.com>
2021-11-08 16:22:48 -0600

    Update main.js

--- CS324E-VSCode/Three.js/main.js
+++ CS324E-VSCode/Three.js/main.js
@@ -1,34 +1,52 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-let camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight, 0.1, 1000);
-let controls = new THREE.OrbitControls(camera);
-camera.position.set(0, 0, 20);
-controls.update();
-
 const renderer = new THREE.WebGLRenderer();
-
 renderer.setSize( window.innerWidth,window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-material.wireframe = true;
-const cube = new THREE.Mesh( geometry, material );
+const scene = new THREE.Scene();
 
-const pivot = new THREE.Object3D();
+// camera init
+const camera = new THREE.PerspectiveCamera(50,window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 100);
+scene.add(camera);
 
-pivot.add( cube );		
-scene.add( pivot );
+// orbit controls
+let controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.update();
 
-camera.position.z = 5;
-cube.position.x = 1;
+//floor
+
+const floor_geometry = new THREE.PlaneGeometry( 100, 100, 1, 1 );
+const floor_material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const floor_mesh = new THREE.Mesh( floor_geometry, floor_material );
+floor_mesh.rotation.x = 90;
+floor_mesh.rotation.y = 0;
+floor_mesh.rotation.z = 0;
+const floor = new THREE.Object3D();
+floor.add(floor_mesh);
+
+//cube 1: green
+const cube_geometry = new THREE.BoxGeometry(40, 40, 40);
+const cube_material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+const cube_mesh = new THREE.Mesh( cube_geometry, cube_material );
+const cube = new THREE.Object3D();
+cube.rotation.x = 0;
+cube.rotation.y = 0;
+cube.rotation.z = 0;
+cube.add( cube_mesh );	
+
+scene.add( floor_mesh );
+scene.add( cube );
 
 draw = function () 
 {
 	requestAnimationFrame( draw );
-	renderer.render( scene, camera );
-	pivot.rotation.x += 0.01;
-	pivot.rotation.y += 0.01;
+
 	controls.update();
+
+	renderer.render( scene, camera );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+	
 };
 draw();
